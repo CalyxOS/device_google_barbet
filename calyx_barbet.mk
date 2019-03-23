64f970fb302afd52f077167a79a37e261033e23d
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit d0a7baabccd3e2f4142453c799b67197dcf01ed6
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:16:43 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637
        (cherry picked from commit 4c5cd21ec3e76def85dec31b7c06b37e2e68d943)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit a00d51ec8fb127a3b13e5101ac9347b87e962cf1)

---
+++ calyx_barbet.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/redbull/device-calyx.mk)
+$(call inherit-product, device/google/barbet/aosp_barbet.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_barbet
+PRODUCT_MODEL := Pixel 5a
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=barbet \
+    PRIVATE_BUILD_DESC="barbet-user 13 TP1A.220624.014 8672226 release-keys"
+
+BUILD_FINGERPRINT := google/barbet/barbet:13/TP1A.220624.014/8672226:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
