4c5cd21ec3e76def85dec31b7c06b37e2e68d943
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-12 03:16:43 +0530

    calyx: Inherit device-calyx.mk
    Change-Id: Ic73a5b0f4108f0f27ed615cc8f2ab2367cb73637

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, vendor/calyx/config/common.mk)
 
 # Inherit device configuration
+$(call inherit-product, device/google/redbull/device-calyx.mk)
 $(call inherit-product, device/google/barbet/aosp_barbet.mk)
 
 ## Device identifier. This must come after all inclusions
