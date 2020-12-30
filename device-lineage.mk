6d88160bf9aa76fb79b6ac8ad4fa0f2420557b2f
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:28 +0530

    barbet: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Calyx Institute
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,6 +9,10 @@ $(call inherit-product, device/google/redbull/device-lineage.mk)
 
 # CHRE
 $(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/barbet:libadsprpc)
+
+# Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/barbet/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
 # HBM
 PRODUCT_PACKAGES += \
