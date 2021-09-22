7c6f7dd7ba4157308b958f37bcb4eea008051590
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:11:38 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -11,6 +11,12 @@ PRODUCT_NAME := calyx_barbet
 PRODUCT_MODEL := Pixel 5a
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=barbet \
+    PRIVATE_BUILD_DESC="barbet-user 11 RD2A.210905.002 7513089 release-keys"
+
+BUILD_FINGERPRINT := google/barbet/barbet:11/RD2A.210905.002/7513089:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/barbet
