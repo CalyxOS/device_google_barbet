5bcead757328c744f5c7473a716e1de012c3d5c2
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-23 16:51:05 +0530

    calyx: Include common device makefile first, so we can override things
    Change-Id: Id8e01814bd14741e5400ed9fb4ff1cccf350637b

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, vendor/calyx/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/google/barbet/device-calyx.mk)
 $(call inherit-product, device/google/redbull/device-calyx.mk)
+$(call inherit-product, device/google/barbet/device-calyx.mk)
 $(call inherit-product, device/google/barbet/aosp_barbet.mk)
 
 ## Device identifier. This must come after all inclusions
