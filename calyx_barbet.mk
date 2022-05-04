0e43236476ff92bba53c8f1edd38b8484d7f785f
Chirayu Desai <chirayudesai1@gmail.com>
2022-05-04 13:57:59 +0530

    Update build fingerprint and desc, May 2022
    Change-Id: Iead2cbf1506e29f42ced129d07d74630cb1e57d6

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 12 SP2A.220405.003 8210211 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 12 SP2A.220505.002 8353555 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:12/SP2A.220405.003/8210211:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:12/SP2A.220505.002/8353555:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
