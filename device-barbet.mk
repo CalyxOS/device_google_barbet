bb11ab678390625b8e62ab54e2fa6b59428cef6c
Michael Bestas <mkbestas@gmail.com>
2023-05-02 19:14:39 +0300

    Always use vintf prebuilts
    Change-Id: I6bc82627c9de9e1fc2974b01e8874a6b9a672c75

--- device-barbet.mk
+++ device-barbet.mk
@@ -17,11 +17,7 @@
 PRODUCT_HARDWARE := barbet
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-        LOCAL_KERNEL := device/google/redbull-kernel/Image.lz4
-    else
-        LOCAL_KERNEL := device/google/redbull-kernel/vintf/Image.lz4
-    endif
+    LOCAL_KERNEL := device/google/redbull-kernel/vintf/Image.lz4
 else
     LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
