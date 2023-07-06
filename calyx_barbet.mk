f11ad2b7c38c6a316101935e74ff36129f22a755
Chirayu Desai <chirayudesai1@gmail.com>
2023-07-06 17:29:12 +0530

    Update fingerprint/build description from TQ3A.230705.001
    Change-Id: I3ab0ce1461a0d0dae6023e6bea378a05badc6d55

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 13 TQ3A.230605.011 10161073 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 13 TQ3A.230705.001 10216780 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:13/TQ3A.230605.011/10161073:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:13/TQ3A.230705.001/10216780:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
