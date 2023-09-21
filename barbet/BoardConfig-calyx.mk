3fe168bc3726c93cd686d7cb5757ec0d0c3561a4
Michael Bestas <mkbestas@gmail.com>
2023-09-21 19:28:44 +0300

    Set security patch levels
    Change-Id: I0d7c997221eab361c4566f73562bab2336085334

--- barbet/BoardConfig-calyx.mk
+++ barbet/BoardConfig-calyx.mk
@@ -6,4 +6,8 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
+BOOT_SECURITY_PATCH := 2023-10-05
+PLATFORM_SECURITY_PATCH := 2023-10-05
+VENDOR_SECURITY_PATCH := 2023-10-05
+
 include vendor/google/barbet/BoardConfigVendor.mk
