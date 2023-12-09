c277bde9dae6a70188b35bd0cd1948458c4287ce
Michael Bestas <mkbestas@gmail.com>
2023-12-09 17:31:58 +0200

    Update security patch level from UQ1A.231205.014
    Change-Id: I242038dccaff730e51462ff26196556a2e3536f9

--- barbet/BoardConfig-calyx.mk
+++ barbet/BoardConfig-calyx.mk
@@ -6,8 +6,8 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
-BOOT_SECURITY_PATCH := 2023-11-05
-PLATFORM_SECURITY_PATCH := 2023-11-05
-VENDOR_SECURITY_PATCH := 2023-11-05
+BOOT_SECURITY_PATCH := 2023-12-05
+PLATFORM_SECURITY_PATCH := 2023-12-05
+VENDOR_SECURITY_PATCH := 2023-12-05
 
 include vendor/google/barbet/BoardConfigVendor.mk
