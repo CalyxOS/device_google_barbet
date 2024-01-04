23cabb19da850b57f2f33f50f265824d087cdad5
Chirayu Desai <chirayudesai1@gmail.com>
2024-01-04 15:21:03 +0530

    Update security patch level from UQ1A.240105.002
    Change-Id: I1cd4ee63c2198b535b7757dfd8cb42b707e6c616

--- barbet/BoardConfig-calyx.mk
+++ barbet/BoardConfig-calyx.mk
@@ -6,8 +6,8 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
-BOOT_SECURITY_PATCH := 2023-12-05
-PLATFORM_SECURITY_PATCH := 2023-12-05
-VENDOR_SECURITY_PATCH := 2023-12-05
+BOOT_SECURITY_PATCH := 2024-01-05
+PLATFORM_SECURITY_PATCH := 2024-01-05
+VENDOR_SECURITY_PATCH := 2024-01-05
 
 include vendor/google/barbet/BoardConfigVendor.mk
