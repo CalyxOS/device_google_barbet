35ca73f9204b912868cf15794d7edbad2dcf52f9
Chirayu Desai <chirayudesai1@gmail.com>
2024-05-08 03:38:56 +0530

    Update security patch level from AP1A.240505.004
    Change-Id: I813c0c6010529eb75c93cfc4054e198d37967600

--- barbet/BoardConfig-calyx.mk
+++ barbet/BoardConfig-calyx.mk
@@ -6,8 +6,8 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
-BOOT_SECURITY_PATCH := 2024-04-05
-PLATFORM_SECURITY_PATCH := 2024-04-05
-VENDOR_SECURITY_PATCH := 2024-04-05
+BOOT_SECURITY_PATCH := 2024-05-05
+PLATFORM_SECURITY_PATCH := 2024-05-05
+VENDOR_SECURITY_PATCH := 2024-05-05
 
 include vendor/google/barbet/BoardConfigVendor.mk
