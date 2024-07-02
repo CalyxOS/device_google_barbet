7d0a30afeeef8301fd7935ecef69c8781e69a2b3
Chirayu Desai <chirayudesai1@gmail.com>
2024-07-03 03:02:40 +0530

    Update fingerprint/build description from AP2A.240705.004
    Change-Id: I046f57557898ad3ae393a5ae57b130e28ba54c5f

--- calyx_barbet.mk
+++ calyx_barbet.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 14 AP2A.240605.024 11860263 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 14 AP2A.240705.004 11875680 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:14/AP2A.240605.024/11860263:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:14/AP2A.240705.004/11875680:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
