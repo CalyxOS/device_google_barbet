e4846c625f123be93119dbe4c36c595e8d280c16
Chirayu Desai <chirayudesai1@gmail.com>
2024-08-06 18:03:26 -0400

    RIP barbet: Final security patch level update from AP2A.240805.005
    Change-Id: Ic1b6a3add6be30d32a3d3642204cab345757b789

--- barbet/BoardConfig-calyx.mk
+++ barbet/BoardConfig-calyx.mk
@@ -6,8 +6,8 @@
 
 include device/google/redbull/BoardConfig-calyx.mk
 
-BOOT_SECURITY_PATCH := 2024-07-05
-PLATFORM_SECURITY_PATCH := 2024-07-05
-VENDOR_SECURITY_PATCH := 2024-07-05
+BOOT_SECURITY_PATCH := 2024-08-05
+PLATFORM_SECURITY_PATCH := 2024-08-05
+VENDOR_SECURITY_PATCH := 2024-08-05
 
 include vendor/google/barbet/BoardConfigVendor.mk
