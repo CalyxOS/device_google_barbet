37ba7263569b46ffe804b4b8d0cab60305e136fb
Michael Bestas <mkbestas@lineageos.org>
2024-08-28 10:36:32 +0300

    barbet: Sync extract scripts with templates
    Change-Id: I83561b9064f7f27af90e21ac72a9c76e1dad8fad

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -34,13 +33,13 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt" true
 write_makefiles "${MY_DIR}/proprietary-files-vendor.txt" true
 
-append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
-
-bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
-
 write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
 write_single_product_copy_files "product/etc/apns-conf.xml"
 write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
 
 # Finish
 write_footers
