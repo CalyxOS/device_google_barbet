558cfe68ddf61b033814a011de2ff1d5d878e445
Michael Bestas <mkbestas@gmail.com>
2025-03-10 22:34:53 +0200

    barbet: Set chre_daemon_dsp_library
    Change-Id: If0a2be3eee2f55fcbb48ca9118f4f14f68ac1c32

--- device-calyx.mk
+++ device-calyx.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# CHRE
+$(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/barbet:libadsprpc)
+
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/barbet/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
